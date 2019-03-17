89e6208c1b8852197511b00c05e956f8b31e6c6d
Heini Ahokas <ahokas.heini@gmail.com>
2019-03-17 16:02:07 +0100

    Set up express

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    const htmlPath = path.join(__dirname, 'public', 'index.html');
+    res.sendFile(htmlPath);
+  });
+
+
+app.set('port', process.env.PORT || 4100);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Magic happening on → PORT ${server.address().port}`);
+});
